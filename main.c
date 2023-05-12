5041e6f191443fa9f5eccca8bc77d563760f0400
Hordunlarmy <Hordunlarmy@gmail.com>
2023-05-12 17:39:52 +0100

    Add main function and header file to project

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	char *shellPrompt = "#DoingHardThings $ ";
+	char *command;
+	size_t n = 0;
+	ssize_t r_value = 0;
+
+	for (; ;)
+	{
+		printf("%s", shellPrompt);
+		r_value = getline(&command, &n, stdin);
+
+		if (r_value == -1)
+			return (-1);
+		printf("%s", command);
+	}
+	free(command);
+
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define _POSIX_C_SOURCE 200809L
+
+/* standard library functions */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#endif /* MAIN_H */
