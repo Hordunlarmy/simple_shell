b6b94d4ca77eb12844daa9efe2296b581f282d15
peter <peterekwere04@gmail.com>
2023-05-13 06:54:48 -0700

    modified Main.h

--- main.h
+++ main.h
@@ -10,7 +10,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/** Global environment */
 extern char **environ;
+
+
+
+
+/**
+ * struct linked_path - This is a linked list containing
+ * ---------------------Each diretory in the path
+ * @dir: This is a directory Path
+ * @next: is apointer to the next directory
+ */
+typedef struct linked_path
+{
+	char *dir;
+	struct linked_path *next;
+} linked_path;
+
+
+
+
+char *_strtok(char *str, const char *delim);
+char *_getenv(const char *name);
+linked_path *link_path(void);
 pid_t execute(char *command);
 
 
