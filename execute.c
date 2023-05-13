e878a9ce01ab173caa3ce6e92fc19003226779a7
peter <peterekwere04@gmail.com>
2023-05-13 01:48:59 -0700

    changed fork.c to execute.c

---
+++ execute.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * execute - Entry point
+ * @command: command passed
+ * Return: id of fork
+ */
+pid_t execute(char *command)
+{
+	char *args[10];
+	pid_t pid;
+	int i = 1;
+
+	pid = fork();
+
+	if (pid == -1)
+		exit(EXIT_FAILURE);
+
+	if (pid == 0)
+	{
+		args[0] = strtok(command, " \n");
+
+		while (args[i] != NULL && i < 10 - 1)
+		{
+			args[i] = strtok(NULL, " \n");
+			i++;
+		}
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("No such file or directory");
+			return (1);
+		}
+	}
+
+	if (pid != 0)
+	{
+		wait(NULL);
+	}
+	return (pid);
+}
