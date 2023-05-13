3b1c52a6d5b0dd56cbf6f458a302aeba2eb53be3
peter <peterekwere04@gmail.com>
2023-05-13 06:55:55 -0700

    created the custom getenv syscall

---
+++ _getenv.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _getenv - is a function that checks for
+ * a variable in the environment
+ * @name: is a a pointer to an array of characters
+ * Return: a pointer if success or NULL if failed
+ */
+
+
+char *_getenv(const char *name)
+{
+	int i = 0;
+	char *envi_ron;
+	char *equals;
+
+	while (environ[i] != NULL)
+	{
+		envi_ron = environ[i];
+		equals = strchr(envi_ron, '=');
+
+		*equals = '\0';
+		if (strcmp(envi_ron, name) == 0)
+		{
+			*equals = '=';
+			return (environ[i]);
+		}
+		*equals = '=';
+		i++;
+	}
+	return (NULL);
+}
