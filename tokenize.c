43e5f89755066090b810d9e4e132dda9a93729d3
Hordunlarmy <Hordunlarmy@gmail.com>
2023-05-14 20:32:39 +0100

    Refactor execute function to take a string argument instead of a string array

--- tokenize.c
+++ tokenize.c
@@ -2,30 +2,13 @@
 
 /**
  * tokenize - Entry point
-<<<<<<< HEAD
  * @command: string passed
  * @args: arguments passed
-=======
- * @command: argument line
- * @args: argument parsed
->>>>>>> e466b39a7e900e2416af90a0f7db4f27837ce0a0
  * Return: Always 0 (Success)
  */
 int tokenize(char *command, char **args)
 {
 	int i = 0;
-<<<<<<< HEAD
-	char *token = strtok(command, " \n");
-
-	while (token != NULL && i < 10 - 1)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " \n");
-	}
-	args[i] = NULL;
-
-	return (i);
-=======
 	char *token = str_tok(command, " \n");
 
 		while (token != NULL && i < 10 - 1)
@@ -34,5 +17,4 @@ int tokenize(char *command, char **args)
 			token = str_tok(NULL, " \n");
 		}
 		return (i);
->>>>>>> e466b39a7e900e2416af90a0f7db4f27837ce0a0
 }
