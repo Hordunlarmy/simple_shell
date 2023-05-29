a08d8581c39e32274e917a159e868d9d93348b75
Hordunlarmy <Hordunlarmy@gmail.com>
2023-05-29 22:04:30 +0100

    Rename task 14 test files to checker/ & update paths

--- checker/task14/check.sh
+++ checker/task14/check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+path="~/simple_shell/hsh"
 chmod u+x checker.bash
 echo -e "\e[35m                   >>>>>>>>> TASK 14 <<<<<<<<<<<\e[0m"
 echo -e "\e[35m                   .............................\e[0m\n"
@@ -8,25 +9,25 @@ echo -e "\n\e[32mCheck 0. README is present and not empty\e[0m"
 echo -e "\n\e[32mCheck 1. Compile program\e[0m"
 
 echo -e "\n\e[32mCheck 2. case: Execute a command that fails, and display its status with echo $?\e[0m"
-./checker.bash ././hsh echo_status_fail.bash
+./checker.bash $path echo_status_fail.bash
 
 echo -e "\n\e[32mCheck 3. case: Execute /bin/ls $HBTN_DIR where HBTN_DIR is “/var”\e[0m"
-./checker.bash ././hsh ls_var.bash
+./checker.bash $path ls_var.bash
 
 echo -e "\n\e[32mCheck 4. case: Execute a command that succeeds and display its status with echo $?\e[0m"
-./checker.bash ././hsh echo_status.bash
+./checker.bash $path echo_status.bash
 
 echo -e "\n\e[32mCheck 5. case: Set the variable HBTN_ECHO to be /bin/echo, and execute $HTBN_ECHO hbtn\e[0m"
-./checker.bash ././hsh echo_as_var.bash
+./checker.bash $path echo_as_var.bash
 
 echo -e "\n\e[32mCheck 6. case: Execute echo $HBTN_UNDEF where HBTN_UNDER does not exist\e[0m"
-./checker.bash ././hsh echo_unknown_var.bash
+./checker.bash $path echo_unknown_var.bash
 
 echo -e "\n\e[32mCheck 7. case: Display shell PID with echo $$\e[0m"
-./checker.bash ././hsh echo_pid.bash
+./checker.bash $path echo_pid.bash
 
 echo -e "\n\e[32mCheck 8. case: Execute echo 3 times, one of them with $ $ (no variable)\e[0m"
-./checker.bash ././hsh echo_pid_fake.bash
+./checker.bash $path echo_pid_fake.bash
 
 echo -e "\n\e[32mCheck 9. case: Execute echo $HBTN_VAR where HBTN_VAR contains a string\e[0m"
-./checker.bash ././hsh echo_var.bash
+./checker.bash $path echo_var.bash
--- checker/task4/check.sh
+++ checker/task4/check.sh
@@ -8,7 +8,7 @@ echo -e "\n\e[32mCheck 0. README is present and not empty\e[0m"
 echo -e "\n\e[32mCheck 1. Compile program\e[0m"
 
 echo -e "\n\e[32mCheck 2. Exit without parameter\e[0m"
-./checker.bash ~/simple_shell/hsh exit_no_arg.bash
+./checker.bash ./hsh exit_no_arg.bash
 
 echo -e "\n\e[32mCheck 3. case: Execute a command that fails, and exit without parameter\e[0m"
-./checker.bash ~/simple_shell/hsh exit_no_arg_1.bash
+./checker.bash ./hsh exit_no_arg_1.bash
